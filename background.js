77f011368b83194ab7228f1b7336c36483a67d80
Flowers Safety <40meta@gmail.com>
2022-11-16 18:55:18 +0600

    hacks does not work!

--- background.js
+++ background.js
@@ -9,12 +9,22 @@ chrome.contextMenus.create(
   {
     "title": "cut"
     , "id": "the fuck"
+    , "contexts" : ["page"]
   },
   () => {console.log("created?");}
 )
 
 chrome.contextMenus.onClicked.addListener((item, tab) => {
   //const item = item.menuItemId
-  console.log(["clicked", item]);
+  //console.log(["clicked", item, tab]);
+  
+  chrome.tabs.sendMessage(
+    tab.id,
+    "getClickedEl",
+    {frameId: item.frameId},
+    el => {
+      //elt.value = data.value;
+      console.log(["background.js sendMessage's callback gets", el]);
+    });
 });
 
---
+++ scripts/some.js
@@ -0,0 +1,23 @@
+let clickedEl = null;
+
+// can I just send dom-node in massage parameter?
+// https://developer.chrome.com/docs/extensions/reference/runtime/#event-onMessage
+document.addEventListener(
+    "contextmenu",
+    function(event){
+        clickedEl = event.target;
+        console.log(["eventListener from some.js", event.target])
+    },
+    true
+);
+
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        if(message == "getClickedEl") {
+            console.log("equals!", clickedEl, message);
+
+            //sendResponse("PLUH!11");
+            sendResponse(clickedEl.textContent);
+        }
+    }
+);
