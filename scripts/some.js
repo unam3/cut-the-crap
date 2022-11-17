a45ffc1d3a62b0357acd75c23e850c35c6301c96
Flowers Safety <40meta@gmail.com>
2022-11-17 15:44:19 +0600

    capture of ctrl + left click was implemented

--- scripts/some.js
+++ scripts/some.js
@@ -3,21 +3,12 @@ let clickedEl = null;
 // can I just send dom-node in massage parameter?
 // https://developer.chrome.com/docs/extensions/reference/runtime/#event-onMessage
 document.addEventListener(
-    "contextmenu",
+    "click",
     function(event){
-        clickedEl = event.target;
-        console.log(["eventListener from some.js", event.target])
+        if (event.ctrlKey) {
+            clickedEl = event.target;
+            console.log(["eventListener from some.js", event.target])
+        }
     },
     true
-);
-
-chrome.runtime.onMessage.addListener(
-    function(message, sender, sendResponse) {
-        if(message == "getClickedEl") {
-            console.log("equals!", clickedEl, message);
-
-            //sendResponse("PLUH!11");
-            sendResponse(clickedEl);
-        }
-    }
 );
