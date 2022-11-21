554b0ad57026163739381f87f2663a50761833e1
Flowers Safety <40meta@gmail.com>
2022-11-21 19:17:33 +0600

    storing modified content was implemented

--- scripts/some.js
+++ scripts/some.js
@@ -1,25 +1,45 @@
 'use strict';
 
+const url = document.location.host.concat(document.location.pathname, document.location.search);
+
+const changes = {};
+
+const generateCSSSelector = (node) => CssSelectorGenerator.getCssSelector(
+    node,
+    {"blacklist": ['*ontenteditable*']}
+);
+
+                          
+const maybeStoreChanges = (original, maybeModified, selector) => {
+    if (original != maybeModified) {
+        changes[selector] = maybeModified;
+        
+        console.log("textContent was stored for ".concat(selector));
+    } else {
+        console.log("textContent is equal to original");
+    }
+}
+
 (async () => {
+
+  // TODO: add hot reloading with https://www.npmjs.com/package/mv3-hot-reload
+  //const hot-reload_src = chrome.runtime.getURL('scripts/.js');
+
+  //const contentScript = await import(css-selector-generator_src);
+
+
+  window.addEventListener(
+      "beforeunload",
+      () => {
+          window.localStorage.setItem(url, JSON.stringify(changes));
+      }
+  );
+
 
   const src = chrome.runtime.getURL('scripts/css-selector-generator.js');
   
   const contentScript = await import(src);
   
-  
-  const logSelector = (additionalInfo) =>
-      console.log(
-        [
-            CssSelectorGenerator.getCssSelector(
-                event.target,
-                {"blacklist": ['*ontenteditable*']}
-            ),
-            additionalInfo,
-            document.location.host.concat(document.location.pathname, document.location.search)
-        ]
-        //CssSelectorGenerator.getCssSelector(event.target)
-      );
-
   document.addEventListener(
       "click",
       function(event) {
@@ -27,16 +47,25 @@
               
               const clicked = event.target;
   
+              const originalTextContent = event.target.textContent;
+
               clicked.contentEditable = true;
               
               clicked.focus();
+
+
+              const selector = generateCSSSelector(event.target);
+
+              console.log(selector, "on click: set to true");
               
               event.target.addEventListener(
                   "focusout",
                   ((event) => {
                       event.target.contentEditable = false;
 
-                      logSelector("set to false");
+                      maybeStoreChanges(originalTextContent, event.target.textContent, selector);
+                      
+                      console.log(selector, changes, "on focusout: set to false");
                   }),
                   true
               );
@@ -49,7 +78,9 @@
                       if (event.key == "Escape") {
                           event.target.contentEditable = false;
 
-                          logSelector("set to false");
+                          maybeStoreChanges(originalTextContent, event.target.textContent, selector);
+                          
+                          console.log(selector, changes, "on Escape keydown: set to false");
                       };
                   }),
                   true
